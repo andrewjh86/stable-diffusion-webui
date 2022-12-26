7ac7a035c804628b60cd90769b115cb10480e2cd
Andrew Herrera <andrewjh8866@yahoo.com>
2022-12-26 16:55:06 -0500

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=  --disable-safe-unpickle  --force-enable-xformers
+set COMMANDLINE_ARGS=  --disable-safe-unpickle 
+REM --force-enable-xformers
 
 call webui.bat
